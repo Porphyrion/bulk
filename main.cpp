456feb58068a11d0afc2a0aae71edad80874baa9
Ivan <ivanbasurman88@yandex.ru>
2018-03-06 14:18:31 +0300

    edit CMakeLists

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@ int main(int argc, char const *argv[]) {
     bulk::CoutObserver ob(cml);
     bulk::LogObserver lo(cml);
 
-    std::cout<<std::endl;
-
     std::string line;
     while(std::getline(std::cin, line)){
         cml->Commnand(line);
