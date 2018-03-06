bd1fdb197b6fea9d326c969d52fba096452f548b
Ivan <ivanbasurman88@yandex.ru>
2018-03-06 19:12:18 +0300

    edit observer

--- log_observer.cpp
+++ log_observer.cpp
@@ -1,12 +1,12 @@
 #include "bulk.h"
 
 void bulk::LogObserver::update(int s){
-    if(s == start){
+    if(s == START){
         bulkBeginTime = boost::lexical_cast<std::string>(time(nullptr));
         bulkFileName = bulkBeginTime;
         bulkFileName.append(".log");
     }
-    else if(s == stop){
+    else if(s == STOP){
         std::ofstream bulkFile(bulkFileName);
         for(auto i : cm->commandBlock){
             bulkFile<<i<<" ";
