5bfa4e32c8f08fe89f13755d171a449781b9a0a2
Ivan <ivanbasurman88@yandex.ru>
2018-03-14 23:06:20 +0300

    edit log observer

--- log_observer.cpp
+++ log_observer.cpp
@@ -7,9 +7,12 @@ void bulk::LogObserver::update(int s){
         bulkFileName.append(".log");
     }
     else if(s == STOP){
-        std::ofstream bulkFile(bulkFileName);
+        std::ofstream bulkFile(bulkFileName, std::ios::out | std::ios::app);
+        bulkFile<<"bulk: ";
         for(auto i : cb->commands){
             bulkFile<<i<<" ";
         }
+        bulkFile<<std::endl;
+        bulkFile.close();
     }
 };
